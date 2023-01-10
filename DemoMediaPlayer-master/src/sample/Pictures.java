31dd5a5660a4420d0267bd97691b50b601c47813
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-01-10 09:45:47 +0100

    Merge branch 'Billeder' of C:\Users\Rasmus T. Hermansen\Documents\GitHub\musik with conflicts.

--- DemoMediaPlayer-master/src/sample/Pictures.java
+++ DemoMediaPlayer-master/src/sample/Pictures.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Pictures
 {
     ArrayList<String> pictures = new ArrayList<>();
-    public void addPictures()
+    public ArrayList<String> addPictures()
     {
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/download (1).jfif");
 
+        return pictures;
     }
 }
