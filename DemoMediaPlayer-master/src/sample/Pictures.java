a78393997bcbdb78b9633df807f9c04ce372784c
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-01-11 10:05:10 +0100

    Added flere billeder

--- DemoMediaPlayer-master/src/sample/Pictures.java
+++ DemoMediaPlayer-master/src/sample/Pictures.java
@@ -4,18 +4,24 @@ import java.util.ArrayList;
 
 public class Pictures
 {
-    Pictures()
-    {
-        System.out.println("Hej");
-    }
+
      static ArrayList<String> pictures = new ArrayList<>();
+    //region addPicures
     public static ArrayList<String> addPictures()
     {
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 2.png");
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/Billed 1.png");
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billeder 3.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/Billed 4.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 5.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 6.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 7.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 8.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 9.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 10.png");
 
 
       return pictures;
     }
+    //region end
 }
