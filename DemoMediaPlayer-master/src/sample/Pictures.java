d6d19567d92d23ad55d728a1a7467b506bb5dac6
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-01-11 10:19:30 +0100

    Added flere billeder 1.1

--- DemoMediaPlayer-master/src/sample/Pictures.java
+++ DemoMediaPlayer-master/src/sample/Pictures.java
@@ -6,7 +6,6 @@ public class Pictures
 {
 
      static ArrayList<String> pictures = new ArrayList<>();
-    //region addPicures
     public static ArrayList<String> addPictures()
     {
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 2.png");
@@ -19,9 +18,11 @@ public class Pictures
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 8.png");
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 9.png");
         pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 10.png");
-
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 11.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 12.png");
+        pictures.add("DemoMediaPlayer-master/src/sample/Billeder/billede 13.png");
 
       return pictures;
     }
-    //region end
+
 }
